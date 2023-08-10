b7f5a5165680b6446233297c79f124373fd67913
Adithya Ajith <50308085+adithyajith925@users.noreply.github.com>
2023-08-10 16:49:45 -0400

    testing moving average strategies

--- Code/exploration/plotting.py
+++ Code/exploration/plotting.py
@@ -3,8 +3,9 @@ import plotly.graph_objects as go
 
 class CandlePlot:
 
-    def __init__(self, df) -> None:
+    def __init__(self, df, candles=True) -> None:
         self.df_plot = df.copy()
+        self.candles = candles
         self.create_candle_fig()
 
     def add_timestr(self):
@@ -14,19 +15,20 @@ class CandlePlot:
     def create_candle_fig(self):
         self.add_timestr()
         self.fig = go.Figure()
-        self.fig.add_trace(go.Candlestick(
-            x = self.df_plot.sTime,
-            open = self.df_plot.mid_o,
-            high = self.df_plot.mid_h,
-            low = self.df_plot.mid_l,
-            close = self.df_plot.mid_c,
-            line=dict(width = 1),
-            opacity = 1,
-            increasing_fillcolor = "#24A06B",
-            decreasing_fillcolor = "#cc2e3c",
-            increasing_line_color = "#2ec886",
-            decreasing_line_color = "#ff3a4c"
-        ))
+        if self.candles:
+            self.fig.add_trace(go.Candlestick(
+                x = self.df_plot.sTime,
+                open = self.df_plot.mid_o,
+                high = self.df_plot.mid_h,
+                low = self.df_plot.mid_l,
+                close = self.df_plot.mid_c,
+                line=dict(width = 1),
+                opacity = 1,
+                increasing_fillcolor = "#24A06B",
+                decreasing_fillcolor = "#cc2e3c",
+                increasing_line_color = "#2ec886",
+                decreasing_line_color = "#ff3a4c"
+            ))
 
     def update_layout(self, width, height, nticks):
         self.fig.update_yaxes(
@@ -47,7 +49,18 @@ class CandlePlot:
             plot_bgcolor = "#2c303c",
             font=dict(size = 8, color = "#e1e1e1")
         )
+
+    def add_traces(self, line_traces):
+        for trace in line_traces:
+            self.fig.add_trace(go.Scatter(
+                x = self.df_plot.sTime,
+                y = self.df_plot[trace],
+                line = dict(width = 2),
+                line_shape = "spline",
+                name = trace
+            ))
     
-    def show_plot(self, width = 900, height = 400, nticks = 5):
+    def show_plot(self, width = 900, height = 400, nticks = 5, line_traces=[]):
+        self.add_traces(line_traces)
         self.update_layout(width, height, nticks)
         self.fig.show()
